b5713ccf1765160290a5e11161dcea9bbfa348f9
wzj <wzj@4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549>
2022-03-15 07:04:46 +0000

    git-svn-id: https://39.108.144.9:8443/svn/puzhen@29 4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549

--- 2.0.0/doc/database/upgrade/v1.1.37.sql
+++
@@ -1,4 +0,0 @@
-
-
-
-INSERT INTO `yoshop_store_access` VALUES ('11003', '数据统计', 'statistics.data/index', '0', '138', '1572507520', '1572507520');
--- 2.0.0/doc/database/upgrade/v1.1.43.sql
+++
@@ -1,2 +0,0 @@
-
-UPDATE `yoshop_store_access` SET `url`='apps.sharing.order.operate/refund' WHERE (`access_id`='10323');
