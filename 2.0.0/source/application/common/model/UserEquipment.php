09c1e3da33075b6bb1b77e7ebb01de3b3fca4709
wzj <wzj@4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549>
2022-05-11 07:09:40 +0000

    git-svn-id: https://39.108.144.9:8443/svn/puzhen@91 4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549

--- 2.0.0/source/application/common/model/UserEquipment.php
+++ 2.0.0/source/application/common/model/UserEquipment.php
@@ -59,13 +59,12 @@ class UserEquipment extends BaseModel
         if($data)
         {
             $diff_days  = diffBetweenTwoDays(date('Y-m-d'),$data['buy_date']);
-            $warranty_setting = SettingModel::getItem('warranty');
-            $warranty_days = $warranty_setting['warranty_days'] - $diff_days;
-            $data['warranty_days'] = $warranty_days > 0 ? $warranty_days : 0;
 
-            $change_days = $warranty_setting['basic_change_days'] + $warranty_setting['change_days'] -  $diff_days;
+            $warranty_days = $data['setting_warranty_days'] - $diff_days;
+            $data['warranty_days_text'] = $warranty_days > 0 ? $warranty_days : 0;
+            $change_days = $data['setting_basic_change_days'] + $data['setting_change_days'] -  $diff_days;
             $data['change_days'] = $change_days > 0 ? $change_days : 0;
-            $data['change_days_text'] = $change_days > 0 ? ($diff_days > $warranty_setting['basic_change_days'] ? '0+'.$change_days : ($warranty_setting['basic_change_days'] - $diff_days) .'+'.$warranty_setting['change_days']) : 0 ;
+            $data['change_days_text'] = $change_days > 0 ? ($diff_days > $data['setting_basic_change_days'] ? '0+'.$change_days : ($data['setting_basic_change_days'] - $diff_days) .'+'.$data['change_days']) : 0 ;
 
         }
         return $data;
