22d978afb550e97c5b1da1f497243132fcf230b0
wzj <wzj@4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549>
2022-09-30 09:20:54 +0000

    git-svn-id: https://39.108.144.9:8443/svn/puzhen@261 4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549

--- 2.0.0/source/application/store/view/tea_qrcode/factory_tea_qrcode/add.php
+++ 2.0.0/source/application/store/view/tea_qrcode/factory_tea_qrcode/add.php
@@ -77,28 +77,30 @@
 												data-am-selected="{searchBox: 0, btnSize: 'sm',
 												 placeholder:'请选择', maxHeight: 400}">
 												 <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                           
-                                            <option value="5" selected >5</option>
-                                         
-                                            <option value="6">6</option>
-                                         
-                                            <option value="7">7</option>
-                                           
-                                            <option value="8">8</option>
-                                          
-                                            <option value="9">9</option>
-                                           
-                                            <option value="11">11</option>
-                                           
-                                            <option value="12">12</option>
-                                            
-                                            <option value="13">13</option>
-                                     
-                                            <option value="14">14</option>
-                                           
-                                            <option value="15">15</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                         
+                          <option value="5" selected >5</option>
+                       
+                          <option value="6">6</option>
+                       
+                          <option value="7">7</option>
+                         
+                          <option value="8">8</option>
+                        
+                          <option value="9">9</option>
+											
+													<option value="10">10</option>
+                         
+                          <option value="11">11</option>
+                         
+                          <option value="12">12</option>
+                          
+                          <option value="13">13</option>
+                   
+                          <option value="14">14</option>
+                         
+                          <option value="15">15</option>
 										       
 										</select>
 										 <label class="am-form-label am-text-left">克</label>
