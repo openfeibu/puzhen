2c5078265d54e01c61c9ae07c6bab0e63d64acd9
wzj <wzj@4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549>
2022-10-17 08:34:12 +0000

    git-svn-id: https://39.108.144.9:8443/svn/puzhen@262 4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549

--- 2.0.0/source/application/common/model/UserEquipment.php
+++ 2.0.0/source/application/common/model/UserEquipment.php
@@ -64,12 +64,30 @@ class UserEquipment extends BaseModel
     }
     public function setWarranty()
     {
-        $diff_days  = diffBetweenTwoDays(date('Y-m-d'),$this['buy_date']);
-        $warranty_days = $this['setting_warranty_days'] - $diff_days;
-        $this['warranty_days_text'] = $warranty_days > 0 ? $warranty_days : 0;
-        $change_days = $this['setting_basic_change_days'] + $this['setting_change_days'] -  $diff_days;
-        $this['change_days'] = $change_days > 0 ? $change_days : 0;
-        $this['change_days_text'] = $change_days > 0 ? ($diff_days > $this['setting_basic_change_days'] ? '0+'.$change_days : ($this['setting_basic_change_days'] - $diff_days) .'+'.($change_days - $this['setting_basic_change_days'])) : 0 ;
-        return $this;
+		    //相隔天数
+		    $diff_days  = diffBetweenTwoDays(date('Y-m-d'),$this['buy_date']);
+		    //剩余保修时间
+		    $warranty_days = $this['setting_warranty_days'] - $diff_days;
+		    $this['warranty_days_text'] = $warranty_days > 0 ? $warranty_days : 0;
+		
+		    //剩余包换: 基础包换 + 赠送包换 - 相隔
+		    $change_days = $this['setting_basic_change_days'] + $this['setting_change_days'] -  $diff_days;
+		    $this['change_days'] = $change_days > 0 ? $change_days : 0;
+		    if($change_days > 0)
+		    {
+			
+				    if($diff_days > $this['setting_basic_change_days'])
+				    {
+					      //已经过了基础包换 0+剩余包换
+					      $this['change_days_text'] = '0+'.$change_days;
+				    }else{
+					      //还没过了基础包换 基础包换-隔离 + 赠送包换
+					      $this['change_days_text'] =  ($this['setting_basic_change_days'] - $diff_days) .'+'.($this['setting_change_days']) ;
+				    }
+		    }else{
+			      $this['change_days_text'] = 0;
+		    }
+		    //$this['change_days_text'] = $change_days > 0 ? ($diff_days > $this['setting_basic_change_days'] ? '0+'.$change_days : ($this['setting_basic_change_days'] - $diff_days) .'+'.($change_days - $this['setting_basic_change_days'])) : 0 ;
+		    return $this;
     }
 }
