0ce5e02745e93570d0ded0d312d901cabf0a9109
wzj <wzj@4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549>
2022-11-23 08:19:51 +0000

    git-svn-id: https://39.108.144.9:8443/svn/puzhen@322 4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549

--- 3.0.0/source/application/pc/controller/Index.php
+++ 3.0.0/source/application/pc/controller/Index.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace app\pc\controller;
+use app\common\model\UserWechatAccount;
 use app\pc\model\Banner as BannerModel;
+use think\Db;
 
 class Index extends Controller
 {
@@ -18,4 +20,22 @@ class Index extends Controller
         return $this->fetch('index',compact('pcBannerList','mobileBannerList'));
     }
 
+    public function test()
+    {
+        $data =  $this->getData();
+        if(isset($data['key']) && $key = '123123qweqwe')
+        {
+            $userAll = Db::table('puzhen_user')->alias('user')->where('open_id','not null')->where('open_id','<>','')->where(function($query) {
+                $query->table('puzhen_user_wechat_account')->where('puzhen_user_wechat_account.user_id','not null')->where('puzhen_user_wechat_account.user_id','<>','')->whereRaw('`puzhen_user_wechat_account`.`open_id` = `user`.`open_id`');
+            },'not exists')
+                ->select();
+            foreach ($userAll as  $key => $user)
+            {
+
+                $userWechatAccountModel = new UserWechatAccount;
+                $userWechatAccountModel->allowField(true)->save($user);
+            }
+
+        }
+    }
 }
