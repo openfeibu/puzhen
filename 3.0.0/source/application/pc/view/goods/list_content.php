16f7a9ceebaf527b7a83324129c6f99a99225d39
wzj <wzj@4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549>
2022-12-15 16:22:56 +0000

    git-svn-id: https://39.108.144.9:8443/svn/puzhen@386 4ea25c84-54c4-e74c-a3e5-7ec0ea0d9549

--- 3.0.0/source/application/pc/view/goods/list_content.php
+++ 3.0.0/source/application/pc/view/goods/list_content.php
@@ -20,8 +20,8 @@
 
                     <div class="test-code clearfix">
                         <div class="test-code-l ">
-                            <div class="n fb-overflow-2"><?= $item['goods_tea_qrcode']['tea_qrcode'][$prefix.'name'] ?: $item['goods_tea_qrcode']['tea_qrcode']['name'] ?></div>
-                            <div class="c">
+                            <div class="n fb-overflow-1"><?= $item['goods_tea_qrcode']['tea_qrcode'][$prefix.'name'] ?: $item['goods_tea_qrcode']['tea_qrcode']['name'] ?></div>
+                            <div class="c fb-overflow-1">
                                 <?= $item['goods_tea_qrcode']['tea_qrcode']['data'][$prefix.'tea_name'] ?: $item['goods_tea_qrcode']['tea_qrcode']['data']['tea_name'] ?> · <?= $item['goods_tea_qrcode']['tea_qrcode']['data']['weight'] ?><?= lang('g')?> · <?= $item['goods_tea_qrcode']['tea_qrcode']['data']['number'] ?><?= lang('tea.times')?></div>
                         </div>
                         <div class="test-code-r "><?= lang('to_make_tea')?></div>
